9e3269e131a66137be775b90bb2050e652f08294
kuri_masa <bigmaron39.56.72@gmail.com>
2022-05-01 19:35:25 +0900

    array,list,dictionary

--- LearningCurve.cs
+++ LearningCurve.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int[] playerScore = { 123, 456, 789 };
+    
+
     void Start()
     {
-        
+        List<string> questPartyMembers = new List<string>()
+        {"March the tief","Corla the Bounty","Serucia the Brave"};
+
+        Debug.LogFormat("Party Member: {0}", questPartyMembers.Count);
+
+        Dictionary<string, int> itemInventory = new Dictionary<string, int>()
+        {
+            {"Potion",5 },
+            {"Antidote",3 },
+            {"Aspilin",1 }
+        };
+
+        Debug.LogFormat("Item:{0}", itemInventory.Count);
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
